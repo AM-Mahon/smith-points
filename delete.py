65ca83b048ec3953da13d55cb4f60558612eb31b
Ann Marie Mahon <annmarie.mahon@colorado.edu>
2020-02-10 22:25:39 -0700

    Remove superfluous print statements

--- delete.py
+++ delete.py
@@ -8,7 +8,6 @@ headers={'content-type':'application/json'}
 with open('delete.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
-        print(row)
         if(row[0] != 'Identikey'):
             url='https://smith-points.herokuapp.com/delete/'+row[0]
             payload={'pass':sys.argv[1]}
--- put.py
+++ put.py
@@ -8,7 +8,6 @@ headers={'content-type':'application/json'}
 with open('points.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
-        print(row)
         if(row[0] != 'Last'):
             url='https://smith-points.herokuapp.com/put/'+row[2]
             payload={'pass':sys.argv[1], 'first':row[1], 'last':row[0], 'points':row[3]}
